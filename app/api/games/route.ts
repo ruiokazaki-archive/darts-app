dec32ef06042dd60390a170081afe5e70e230b37
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-03-27 04:00:18 +0900

    feat: ゲームを登録出来るようにした

--- app/api/games/route.ts
+++ app/api/games/route.ts
@@ -18,6 +18,69 @@ export async function GET() {
 
 export async function POST(request: Request) {
   const formData = await request.formData();
+  const scores = {
+    20: formData.get("target-20"),
+    19: formData.get("target-19"),
+    18: formData.get("target-18"),
+    17: formData.get("target-17"),
+    16: formData.get("target-16"),
+    15: formData.get("target-15"),
+    bull: formData.get("target-bull"),
+  };
+
+  if (
+    Object.values(scores).some(
+      (score) => typeof score !== "string" || isNaN(Number(score))
+    )
+  ) {
+    return new Response(null, { status: 400, statusText: "Bad Request" });
+  }
+
+  const played_at = new Date().toISOString();
+
+  await sql`INSERT INTO games (played_at) VALUES (${played_at});`;
+  const { rowCount, rows } =
+    await sql<Game>`SELECT * FROM games WHERE played_at = ${played_at}`;
+
+  if (rowCount !== 1) {
+    return new Response(null, {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+
+  const newGame = rows[0];
+
+  const result = await Promise.all([
+    sql`INSERT INTO darts_scores (game_id, target, throws) VALUES (${
+      newGame.id
+    }, '20', ${scores["20"] as string});`,
+    sql`INSERT INTO darts_scores (game_id, target, throws) VALUES (${
+      newGame.id
+    }, '19', ${scores["19"] as string});`,
+    sql`INSERT INTO darts_scores (game_id, target, throws) VALUES (${
+      newGame.id
+    }, '18', ${scores["18"] as string});`,
+    sql`INSERT INTO darts_scores (game_id, target, throws) VALUES (${
+      newGame.id
+    }, '17', ${scores["17"] as string});`,
+    sql`INSERT INTO darts_scores (game_id, target, throws) VALUES (${
+      newGame.id
+    }, '16', ${scores["16"] as string});`,
+    sql`INSERT INTO darts_scores (game_id, target, throws) VALUES (${
+      newGame.id
+    }, '15', ${scores["15"] as string});`,
+    sql`INSERT INTO darts_scores (game_id, target, throws) VALUES (${
+      newGame.id
+    }, 'BULL', ${scores["bull"] as string});`,
+  ]);
+
+  if (result.some((r) => r.rowCount !== 1)) {
+    return new Response(null, {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
 
   return Response.json(
     {

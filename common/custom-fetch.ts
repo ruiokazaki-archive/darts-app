0673dad313fc266b4faff3cce9a0fb7a06cd2844
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-03-27 03:40:07 +0900

    feat: fetchのレスポンスにstatusとstatusTextを返すようにした

--- common/custom-fetch.ts
+++ common/custom-fetch.ts
@@ -6,9 +6,18 @@ export async function customFetch<T>(
     method?: "GET" | "POST" | "PUT" | "DELETE";
     body?: BodyInit;
   }
-): Promise<T> {
+): Promise<{
+  status: number;
+  statusText: string;
+  data: T;
+}> {
   const host = headers().get("host");
   const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
   const response = await fetch(`${protocol}://${host}${path}`, options);
-  return response.json() as Promise<T>;
+  const result = {
+    status: response.status,
+    statusText: response.statusText,
+    data: (await response.json()) as T,
+  };
+  return result;
 }

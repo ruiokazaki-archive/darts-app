1d27977a5cfe7ec4aa0d28ab1ebe8e57f18e8d28
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-04-02 02:36:28 +0900

    feat: middlewareを実装

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { type NextRequest, NextResponse, userAgent } from 'next/server';
+
+export default function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const { device } = userAgent(request);
+  const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
+
+  const hasPrefixSp = pathname.startsWith('/sp');
+  const hasPrefixPc = pathname.startsWith('/pc');
+  const hasPrefixSpOrPc = hasPrefixSp || hasPrefixPc;
+
+  if (!hasPrefixSpOrPc && viewport === 'mobile') {
+    return NextResponse.rewrite(new URL('/sp', request.url));
+  }
+
+  if (!hasPrefixSpOrPc && viewport === 'desktop') {
+    return NextResponse.rewrite(new URL('/pc', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/((?!api|_next|static|public|favicon.ico).*)',
+};

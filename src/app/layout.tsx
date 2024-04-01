a812ea09e328933a66d0e3a0c187be8cadefa3c7
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-04-02 02:52:25 +0900

    refactor: 使用しないページのコンテンツを削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,15 @@
 import './globals.css';
 
+// default values
+export const dynamic = 'auto';
+export const dynamicParams = true;
+export const revalidate = false;
+export const fetchCache = 'auto';
+export const preferredRegion = 'auto';
+
+// changed values
+export const runtime = 'edge';
+
 export default function RootLayout({
   children,
 }: {

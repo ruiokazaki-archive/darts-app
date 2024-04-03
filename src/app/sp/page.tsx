e67e48dccb3457eb61d530c7437ac21ee00a90a8
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-04-03 22:12:05 +0900

    chore: コンポーネントに分けた

--- src/app/sp/page.tsx
+++ src/app/sp/page.tsx
@@ -1,14 +1,12 @@
 import { getGames } from '@/app/api/games/fetcher';
 import { Heading } from '@/components/ui/heading';
-import { Sheet } from '@/components/ui/sheet';
-import { Typography } from '@/components/ui/typography';
 import { RatingChart } from '@/features/rate/components/rating-chart';
 import { convertAverageMarksPerRoundToRate } from '@/features/rate/utils';
 import {
   averageMarksPerRound80,
   averageMarksPerRound100,
 } from '@/features/rate/utils/get-average-marks-per-round';
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import { PerformanceOverviewSheets } from './_components/performance-overview-sheets';
 
 const GAMES_INCLUDED_IN_RATE_CALCULATION = 5 as const;
 
@@ -79,83 +77,14 @@ export default async function Page() {
         />
       </div>
 
-      <div className='mx-4 grid h-28 grid-cols-3 gap-2'>
-        <Sheet>
-          <div className='flex flex-col items-center py-4'>
-            <Typography as='h3' variant='table-header'>
-              Throws
-            </Typography>
-            <Typography variant='h2' as='p' className='tracking-normal'>
-              {totalThrowsLatest.toFixed(0)}
-            </Typography>
-            <div className='flex items-center'>
-              {totalThrowsDiff < 0 ? (
-                <ChevronUp className='h-[0.625rem] w-[0.625rem] stroke-lime-200' />
-              ) : (
-                <ChevronDown className='h-[0.625rem] w-[0.625rem] stroke-red-200' />
-              )}
-              <Typography
-                className={
-                  totalThrowsDiff < 0 ? 'text-lime-200' : 'text-red-200'
-                }
-                variant='detail'
-              >
-                {totalThrowsDiff.toFixed(0)}
-              </Typography>
-            </div>
-          </div>
-        </Sheet>
-        <Sheet>
-          <div className='flex flex-col items-center py-4'>
-            <Typography as='h3' variant='table-header'>
-              MPR 80
-            </Typography>
-            <Typography variant='h2' as='p' className='tracking-normal'>
-              {averageMarks80Latest.toFixed(2)}
-            </Typography>
-            <div className='flex items-center'>
-              {averageMarks80Diff > 0 ? (
-                <ChevronUp className='h-[0.625rem] w-[0.625rem] stroke-lime-200' />
-              ) : (
-                <ChevronDown className='h-[0.625rem] w-[0.625rem] stroke-red-200' />
-              )}
-              <Typography
-                className={
-                  averageMarks80Diff > 0 ? 'text-lime-200' : 'text-red-200'
-                }
-                variant='detail'
-              >
-                {averageMarks80Diff.toFixed(2)}
-              </Typography>
-            </div>
-          </div>
-        </Sheet>
-        <Sheet>
-          <div className='flex flex-col items-center py-4'>
-            <Typography as='h3' variant='table-header'>
-              MPR 100
-            </Typography>
-            <Typography variant='h2' as='p' className='tracking-normal'>
-              {averageMarks100Latest.toFixed(2)}
-            </Typography>
-            <div className='flex items-center'>
-              {averageMarks100Diff > 0 ? (
-                <ChevronUp className='h-[0.625rem] w-[0.625rem] stroke-lime-200' />
-              ) : (
-                <ChevronDown className='h-[0.625rem] w-[0.625rem] stroke-red-200' />
-              )}
-              <Typography
-                className={
-                  averageMarks100Diff > 0 ? 'text-lime-200' : 'text-red-200'
-                }
-                variant='detail'
-              >
-                {averageMarks100Diff.toFixed(2)}
-              </Typography>
-            </div>
-          </div>
-        </Sheet>
-      </div>
+      <PerformanceOverviewSheets
+        totalThrowsLatest={totalThrowsLatest}
+        totalThrowsDiff={totalThrowsDiff}
+        averageMarks80Latest={averageMarks80Latest}
+        averageMarks80Diff={averageMarks80Diff}
+        averageMarks100Latest={averageMarks100Latest}
+        averageMarks100Diff={averageMarks100Diff}
+      />
     </main>
   );
 }
